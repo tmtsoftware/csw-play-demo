cdf9ce3d67078e14739a35b49edda540445116b9
Allan Brighton <Allan.Brighton@t-online.de>
2015-11-30 17:07:15 -0800

    telemetry feedback from OPC

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 dir=../install
 
-test -d $dir || mkdir -p $dir/{bin,lib,conf}
+test -d $dir || mkdir -p $dir/bin $dir/lib $dir/conf
 sbt stage
 for i in bin lib ; do cp -f */target/universal/stage/$i/* $dir/$i/; done
 for i in bin lib conf ; do cp -f demo-web-server/target/universal/stage/$i/* $dir/$i/; done
